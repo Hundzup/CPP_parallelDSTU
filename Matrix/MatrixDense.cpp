536cc90eaed5afa966d4cfd3d280213cf546fe52
anima-boy <69791587+anima-boy@users.noreply.github.com>
2024-10-13 17:05:20 +0300

    new funk

--- Matrix/MatrixDense.cpp
+++ Matrix/MatrixDense.cpp
@@ -29,8 +29,35 @@ class MatrixDense{
             if (row < 0 || row > rows || col < 0 || col > cols){
                 throw std::out_of_range("Index out of range");
             }
-            return arr[ros][col];
+            return arr[row][col];
         }
 
-        
+        const T& operator()(size_t row, size_t col){
+            if (row < 0 || row > rows || col < 0 || col > cols){
+                throw std::out_of_range("Index out of range");
+            }
+            return arr[row][col];
+        }
+
+        MatrixDense<T> operator+(const MatrixDense<T>) const{
+            if (rows != other.rows || cols != other.cols){
+                throw std::invalid_argument("Cols and rows have incorrect size");
+            }
+            MatrixDense<T> rezult(rows, cols);
+            for (size_t i = 0; i < rows; i ++){
+                for (size_t j = 0; j < cols; j++){
+                    rezult(i, j) = arr[i][j] + other(i, j);
+                }
+            }
+            return rezult;
+        }
+
+        void print(){
+            for (size_t i = 0; i < rows; i++){
+                for (size_t j = 0; j < cols; j++){
+                    std::cout << arr[i][j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
 };
