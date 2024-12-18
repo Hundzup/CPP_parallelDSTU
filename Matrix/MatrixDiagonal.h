2582c0c2c0983c91d27f8e4313b4fc70ea6e7078
Hundzup <solomakha.05@yandex.ru>
2024-12-18 17:19:46 +0300

    create matrixDiagonal

--- Matrix/MatrixDiagonal.h
+++ Matrix/MatrixDiagonal.h
@@ -7,8 +7,45 @@
 template<typename T>
 class MatrixDiagonal{
     private:
+        size_t size;
+        int* idx;
+        T** arr;
     public:
-    
-}
+        MatrixDiagonal(size_t size, int* idx){
+            
+        }
+
+        ~MatrixDiagonal(){
+
+        }
+
+        T& operator()(size_t row, size_t col){
+
+        }
+
+        MatrixDiagonal<T> operator+(const MatrixDiagonal<T>& other) const{
+
+        }
+
+        MatrixDiagonal<T> operator-(const MatrixDiagonal<T>& other) const{
+
+        }
+
+        MatrixDiagonal<T> operator*(const MatrixDiagonal<T>& other) const{
+
+        }
+
+        MatrixDiagonal<T> transpose() const{
+
+        }   
+
+        void export2file(const char* name){
+
+        }   
+
+        void importfromfile(const char* name){
+            
+        } 
+} 
 
 #endif
